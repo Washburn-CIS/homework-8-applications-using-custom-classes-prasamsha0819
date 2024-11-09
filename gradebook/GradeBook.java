1add80f7d16f0cc6aad21d9a5063e2042c4eb86c
prasamsha0819 <prasamshasingh.thakuri@washburn.edu>
2024-11-09 02:39:51 +0000

    done with the option

--- gradebook/GradeBook.java
+++ gradebook/GradeBook.java
@@ -20,18 +20,47 @@ public class GradeBook {
             System.out.println("\nPlease make a selection:\n");
             System.out.println("1) List Class Grades");
             System.out.println("2) Exit");
+            //another option
+            System.out.println("3) Update grade");
+            System.out.println();
             System.out.print("\nPlease choose an option: ");
             String choice = input.nextLine();
             System.out.println();
             switch(choice) {
                 case "1": // TODO: write a loop that, for each student prints 
                           //       lname, fname: grade 
+                        for (Student student : students){
+                            System.out.printf("%s, %s: %f\n", student.getLastName(), student.getFirstName(), student.getGrade());
+                        }
+
                     continue;
                 case "2":
                     System.out.println("Goodbye!");
                     return;
 
                 // Challenge: write another option to allow updating grades for a selected student
+                case "3":
+                    //updating grade
+                    System.out.println("Please choose 1 to update grades for Alan and choose 2 to update grades for Ada: ");
+                    String updatedgrade = input.nextLine();
+                    if (updatedgrade.equals("1")){
+                        System.out.println("Enter a new Score for Alan");
+                        int newGrade = input.nextInt();
+                        students[0].setGrade(newGrade);
+                        System.out.println("Alan's grade updated successfully.");
+                    }
+                    else if (updatedgrade.equals("2")){
+                         System.out.println("Enter a new Score for Ada");
+                        int newGrade = input.nextInt();
+                        students[1].setGrade(newGrade);
+                        System.out.println("Ada's grade updated successfully.");
+                    }
+                    else{
+                        System.out.println("Invalid choice. Enter either 1 or 2.");
+                    }
+                    
+                    
+
             }
         }
     }
